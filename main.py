2697fb7fba4c2e79c510311bf5a76b748150c7cc
Niklas <niklas@baranskis.de>
2021-09-04 14:51:42 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import datetime
+import pywhatkit as kit
+import pyautogui as auto
+
+#start query (phone + message)
+print("Who do you want to send a message to?")
+phone = input("Telephoner's: ")
+print("What message do you want to write?")
+message = input("Message: ")
+#query time
+e = datetime.datetime.now()
+if e.minute <= 59:
+    minute = e.minute + 1
+else:
+    minute = 0
+if e.second > 45:
+    minute = minute + 10
+print("Your message will be sent in less than a minute.")
+#send message
+kit.sendwhatmsg(phone, message, e.hour, minute)
+#instant spam message
+query = 'yes'
+if query == 'yes':
+    auto.write(message)
+    auto.press('enter')
